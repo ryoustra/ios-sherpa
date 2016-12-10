328d1e178dd6db5de845bee64d726bc791750db0
Daniel Farrelly <daniel@jellystyle.com>
2016-12-10 11:21:32 +1100

    Add testing of parsing from JSON files.

--- src/SherpaTests/DocumentTests.swift
+++ src/SherpaTests/DocumentTests.swift
@@ -27,56 +27,28 @@ import XCTest
 
 class DocumentTests: XCTestCase {
     
-    static let dictionary = [
-        "feedback_email": "JellyStyle Support <support@jellystyle.com>",
-        "feedback_twitter": "jellybeansoup",
-        "entries": [
-            [
-                "detail": "Text that appears in the footer for the section.",
-                "articles": [
-                    [
-                        "title": "Article 1",
-                        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lacinia, nibh in rutrum placerat, augue risus convallis enim, eget elementum metus odio ut arcu. Phasellus ultricies sollicitudin arcu, at lacinia sem convallis sit amet. Suspendisse ac mauris elementum, eleifend est in, placerat urna. Donec elementum dignissim elit sed tempor.",
-                        "build_min": 360,
-                    ],
-                    [
-                        "key": "invalid-article",
-                    ],
-                    [
-                        "key": "article-key",
-                        "title": "Article 2",
-                        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at rhoncus erat, in bibendum lectus. Aliquam vel turpis nec justo volutpat rhoncus. Integer venenatis, lectus sed ultrices dictum, dolor nunc scelerisque arcu, et tincidunt justo elit eu nulla. Aenean erat ante, blandit vitae elit non, posuere eleifend mi.",
-                        "build_min": 401,
-                    ],
-                ],
-            ],
-            [
-                "title": "Empty Section",
-                "articles": [],
-            ],
-            [
-                "title": "Section Three",
-                "articles": [
-                    [
-                        "key": "article-key",
-                        "title": "Article 3",
-                        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla hendrerit tortor ac velit porta, at bibendum neque imperdiet. In bibendum urna nec nisl consequat, ac eleifend lacus vehicula. Nam laoreet risus a mollis ultrices. Proin at magna eget sem interdum porta id eget eros. Nam vitae molestie orci, at ornare mauris. In hac habitasse platea dictumst.",
-                    ],
-                ],
-            ],
-        ],
-    ]
+	static let dictionary: [String:AnyObject] = {
+		let url = NSBundle(forClass: DocumentTests.self).URLForResource("dictionary", withExtension: "json")!
+		let data = NSData(contentsOfURL: url)!
+		return try! NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(rawValue: 0)) as! [String:AnyObject]
+	}()
+	
+	static let array: [[String:AnyObject]] = {
+		let url = NSBundle(forClass: DocumentTests.self).URLForResource("array", withExtension: "json")!
+		let data = NSData(contentsOfURL: url)!
+		return try! NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(rawValue: 0)) as! [[String:AnyObject]]
+	}()
 
 	func testFeedbackEmail() {
 		var dictionary = DocumentTests.dictionary
 		
 		dictionary["feedback_email"] = "support@jellystyle.com"
 		let validEmail = Sherpa.Document(dictionary: dictionary)
-		XCTAssertEqual(validEmail.feedbackEmail, dictionary["feedback_email"], "Feedback email should match the 'feedback_email' value in the provided dictionary.")
+		XCTAssertEqual(validEmail.feedbackEmail, dictionary["feedback_email"] as? String, "Feedback email should match the 'feedback_email' value in the provided dictionary.")
 		
 		dictionary["feedback_email"] = "JellyStyle Support <support@jellystyle.com>"
 		let validEmailWithName = Sherpa.Document(dictionary: dictionary)
-		XCTAssertEqual(validEmailWithName.feedbackEmail, dictionary["feedback_email"], "Feedback email should match the 'feedback_email' value in the provided dictionary.")
+		XCTAssertEqual(validEmailWithName.feedbackEmail, dictionary["feedback_email"] as? String, "Feedback email should match the 'feedback_email' value in the provided dictionary.")
 		
 		dictionary["feedback_email"] = "JellyStyle Support"
 		let emailAsInvalidString = Sherpa.Document(dictionary: dictionary)
@@ -100,7 +72,7 @@ class DocumentTests: XCTestCase {
 		
 		dictionary["feedback_twitter"] = "jellybeansoup"
 		let validTwitter = Sherpa.Document(dictionary: dictionary)
-		XCTAssertEqual(validTwitter.feedbackTwitter, dictionary["feedback_twitter"], "Feedback Twitter account should match the 'feedback_twitter' value in the provided dictionary.")
+		XCTAssertEqual(validTwitter.feedbackTwitter, dictionary["feedback_twitter"] as? String, "Feedback Twitter account should match the 'feedback_twitter' value in the provided dictionary.")
 		
 		dictionary["feedback_twitter"] = "@jellybeansoup"
 		let validTwitterWithAt = Sherpa.Document(dictionary: dictionary)
@@ -127,26 +99,69 @@ class DocumentTests: XCTestCase {
 		XCTAssertNil(missingTwitter.feedbackTwitter, "Feedback Twitter account should match the 'feedback_twitter' value in the provided dictionary.")
 	}
 
+	func testSectionsFromFiles() {
+		let bundle = NSBundle(forClass: DocumentTests.self)
+		
+		let fileURLs: [(url: NSURL, shouldBeValid: Bool)] = [
+			(bundle.URLForResource("dictionary", withExtension: "json")!, true),
+			(bundle.URLForResource("array", withExtension: "json")!, true),
+			(bundle.URLForResource("invalid", withExtension: "json")!, false),
+			(bundle.resourceURL!.URLByAppendingPathComponent("missing.json")!, false),
+		]
+		
+		for (url, shouldBeValid) in fileURLs {
+			let entries = DocumentTests.dictionary["entries"] as! [[String: AnyObject]]
+			let document = Sherpa.Document(fileAtURL: url)
+			
+			if shouldBeValid {
+				XCTAssertEqual(document.sections.count, entries.count - 1, "Number of sections in the document should reflect the number found in the file, minus any empty sections.")
+				XCTAssertEqual(document.sections[0].title, entries[0]["title"] as? String, "Sections in the document should match those found in the file, minus any empty sections.")
+				XCTAssertEqual(document.sections[1].title, entries[2]["title"] as? String, "Sections in the document should match those found in the file, minus any empty sections.")
+			}
+			else {
+				XCTAssertEqual(document.sections.count, 0, "\(url); Number of sections in the document should be zero if the JSON file does not match the required structure.")
+			}
+		}
+	}
+	
 	func testSectionsFromDictionary() {
-        let entries = DocumentTests.dictionary["entries"] as! [[String: AnyObject]]
-        let document = Sherpa.Document(dictionary: DocumentTests.dictionary)
+		var dictionary = DocumentTests.dictionary
+		var entries = DocumentTests.array
+		
+		dictionary["entries"] = entries
+		let validEntries = Sherpa.Document(dictionary: dictionary)
+		XCTAssertEqual(validEntries.sections.count, entries.count - 1, "Number of sections in the document should reflect the number found in the dictionary, minus any empty sections.")
+		XCTAssertEqual(validEntries.sections[0].title, entries[0]["title"] as? String, "Sections in the document should match those found in the dictionary, minus any empty sections.")
+		XCTAssertEqual(validEntries.sections[1].title, entries[2]["title"] as? String, "Sections in the document should match those found in the dictionary, minus any empty sections.")
 
-        XCTAssertEqual(document.sections.count, entries.count - 1, "Number of sections in the document should reflect the number found in the dictionary, minus any empty sections.")
-        XCTAssertEqual(document.sections[0].title, entries[0]["title"] as? String, "Sections in the document should match those found in the dictionary, minus any empty sections.")
-        XCTAssertEqual(document.sections[1].title, entries[2]["title"] as? String, "Sections in the document should match those found in the dictionary, minus any empty sections.")
-    }
-    
+		dictionary["entries"] = []
+		let entriesAsEmptyArray = Sherpa.Document(dictionary: dictionary)
+		XCTAssertEqual(entriesAsEmptyArray.sections.count, 0, "Number of sections in the document should reflect the number found in the dictionary, minus any empty sections.")
+		
+		dictionary["entries"] = ArticleTests.dictionary
+		let entriesAsArticleDictionary = Sherpa.Document(dictionary: dictionary)
+		XCTAssertEqual(entriesAsArticleDictionary.sections.count, 0, "Number of sections in the document should be zero if the JSON file does not match the required structure.")
+		
+		dictionary["entries"] = [23]
+		let entriesAsInvalidType = Sherpa.Document(dictionary: dictionary)
+		XCTAssertEqual(entriesAsInvalidType.sections.count, 0, "Number of sections in the document should be zero if the JSON file does not match the required structure.")
+		
+		dictionary.removeValueForKey("entries")
+		let missingEntries = Sherpa.Document(dictionary: dictionary)
+		XCTAssertEqual(missingEntries.sections.count, 0, "Number of sections in the document should be zero if the JSON file does not match the required structure.")
+	}
+	
     func testSectionsFromArray() {
-        let entries = DocumentTests.dictionary["entries"] as! [[String: AnyObject]]
-        let document = Sherpa.Document(array: entries)
-        
+        let document = Sherpa.Document(array: DocumentTests.array)
+		let entries = DocumentTests.array
+		
         XCTAssertEqual(document.sections.count, entries.count - 1, "Number of sections in the document should reflect the number found in the array, minus any empty sections.")
         XCTAssertEqual(document.sections[0].title, entries[0]["title"] as? String, "Sections in the document should match those found in the array, minus any empty sections.")
         XCTAssertEqual(document.sections[1].title, entries[2]["title"] as? String, "Sections in the document should match those found in the array, minus any empty sections.")
     }
     
     func testSectionAtIndex() {
-        let document = Sherpa.Document(dictionary: DocumentTests.dictionary)
+        let document = Sherpa.Document(array: DocumentTests.array)
         
         XCTAssertNil(document.section(-1), "Nil should be returned when attempting to retrieve section with out-of-bounds index.")
         XCTAssertNil(document.section(100), "Nil should be returned when attempting to retrieve section with out-of-bounds index.")
@@ -154,14 +169,14 @@ class DocumentTests: XCTestCase {
     }
 
     func testArticleWithKey() {
-        let document = Sherpa.Document(dictionary: DocumentTests.dictionary)
+        let document = Sherpa.Document(array: DocumentTests.array)
         
         XCTAssertNil(document.article("invalid-key"), "Nil should be returned when attempting to retrieve article with a key that doesn't exist.")
         XCTAssertEqual(document.article("article-key")?.title, document.sections[0].articles[1].title, "Section retrieved by key should be the first matching when accessing via indices.")
     }
 
     func testDelegate() {
-        let document = Sherpa.Document(dictionary: DocumentTests.dictionary)
+        let document = Sherpa.Document(array: DocumentTests.array)
         let delegate = DocumentTestDelegate()
         
         document.delegate = delegate
